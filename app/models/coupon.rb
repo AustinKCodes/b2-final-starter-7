b0ec46b37cb10d9894bd94609123411149a01407
Austin K <austink@Austins-MacBook-Pro.local>
2024-08-06 12:40:17 -0400

    US1 complete

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -3,9 +3,4 @@ class Coupon < ApplicationRecord
 
   enum discount_type: { percent: 0, dollar: 1 }
 
-  private
-
-  def value_must_be_positive
-    errors.add(:value, "must be greater than 0") if value <= 0
-  end
 end
