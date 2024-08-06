e5ad24228ad72d1fd1d5b9cedfd65af6901ac4a4
Austin K <austink@Austins-MacBook-Pro.local>
2024-08-06 18:01:18 -0400

    Usage count test US3

--- spec/features/coupons/show_spec.rb
+++ spec/features/coupons/show_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe "Coupon Show Page" do
     @merchant1 = Merchant.create!(name: "Hair Care")
 
     @coupon1 = @merchant1.coupons.create!(name: "Coupon 1", code: "CODE1", discount_type: 0, discount_amount: 10, active:true)
+    @coupon2 = @merchant1.coupons.create!(name: "Coupon 2", code: "CODE2", discount_type: 0, discount_amount: 10, active:true)
 
     @customer_4 = Customer.create!(first_name: "Leigh Ann", last_name: "Bron")
     @customer_5 = Customer.create!(first_name: "Sylvester", last_name: "Nader")
@@ -14,7 +15,9 @@ RSpec.describe "Coupon Show Page" do
     @invoice_6 = Invoice.create!(customer_id: @customer_5.id, status: 2)
     @invoice_7 = Invoice.create!(customer_id: @customer_6.id, status: 1)
 
-    
+    @transaction3 = Transaction.create!(credit_card_number: 234092, result: 1, invoice_id: @invoice_4.id)
+    @transaction4 = Transaction.create!(credit_card_number: 230429, result: 1, invoice_id: @invoice_5.id)
+    @transaction5 = Transaction.create!(credit_card_number: 102938, result: 0, invoice_id: @invoice_6.id)
 
     visit merchant_coupons_path(@merchant1)
   end
@@ -26,10 +29,14 @@ RSpec.describe "Coupon Show Page" do
     expect(page).to have_content(@coupon1.code)
     expect(page).to have_content("10% off")
     expect(page).to have_content("Active")
+    expect(page).to_not have_content(@coupon2.name)
   end
 
   it "should count how many coupons have been used" do
+    visit merchant_coupon_path(@merchant1, @coupon1)
 
+    expect(page).to have_content("Use Count: #{@coupon1.use_count}")
+    expect(page).to_not have_content("Use Count: #{@coupon2.use_count}")
   end
 
 end
