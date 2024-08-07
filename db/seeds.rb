e0fe4a47bf43a91a8d31ef51673bf806fc8b7bd9
Austin K <austink@Austins-MacBook-Pro.local>
2024-08-07 04:08:35 -0400

    I hate Heroku and this rake task

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,9 @@
 
 Rake::Task["csv_load:all"].invoke
 
+customer_1 = Customer.create(first_name: "Joey", last_name: "Smith")
+customer_2 = Customer.create(first_name: "Cecilia", last_name: "Jones")
+
 merchant1 = Merchant.create(name: "Poseidon Pools")
 merchant2 = Merchant.create(name: "Hair Care")
 
@@ -19,20 +22,17 @@ item4 = Item.create(name: "Conditioner", description: "Shiny hair maker", unit_p
 coupon1 = Coupon.create(name: "Coupon 1", code: "SAVE10" ,discount_type: "percent", discount_amount: 10, active: true, merchant: merchant1)
 coupon2 = Coupon.create(name: "Coupon 2", code: "FIVEOFF", discount_type: "dollar", discount_amount: 5, active: true, merchant: merchant2)
 
-customer_1 = Customer.create(first_name: "Joey", last_name: "Smith")
-customer_2 = Customer.create(first_name: "Cecilia", last_name: "Jones")
-
 invoice_2 = Invoice.create(customer_id: customer_1.id, status: 2)
 invoice_3 = Invoice.create(customer_id: customer_2.id, status: 2)
 invoice_1 = Invoice.create(customer_id: customer_2.id, status: 2, coupon: coupon1)
 invoice_4 = Invoice.create(customer_id: customer_1.id, status: 2, coupon: coupon2)
 
-ii_1 = InvoiceItem.create(invoice_id: invoice_2, item_id: item1, quantity: 1, unit_price: 5, status: 0)
-ii_2 = InvoiceItem.create(invoice_id: invoice_4, item_id: item2, quantity: 2, unit_price: 10, status: 2)
-ii_3 = InvoiceItem.create(invoice_id: invoice_3, item_id: item3, quantity: 2, unit_price: 10, status: 1)
-ii_4 = InvoiceItem.create(invoice_id: invoice_1, item_id: item4, quantity: 3, unit_price: 10, status:2)
-
 transaction1 = Transaction.create(credit_card_number: 243155, result: 1, invoice_id: invoice_2.id)
 transaction2 = Transaction.create(credit_card_number: 243155, result: 1, invoice_id: invoice_4.id)
 transaction3 = Transaction.create(credit_card_number: 246151, result: 1, invoice_id: invoice_3.id)
 transaction4 = Transaction.create(credit_card_number: 246151, result: 1, invoice_id: invoice_1.id)
+
+ii_1 = InvoiceItem.create(invoice_id: invoice_2, item_id: item1, quantity: 1, unit_price: 5, status: 0)
+ii_2 = InvoiceItem.create(invoice_id: invoice_4, item_id: item2, quantity: 2, unit_price: 10, status: 2)
+ii_3 = InvoiceItem.create(invoice_id: invoice_3, item_id: item3, quantity: 2, unit_price: 10, status: 1)
+ii_4 = InvoiceItem.create(invoice_id: invoice_1, item_id: item4, quantity: 3, unit_price: 10, status:2)
